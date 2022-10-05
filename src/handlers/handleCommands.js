c996f6aa946e7a6486fd1183bd2aace833ce3070
Annoying <84549791+Anno-Ying@users.noreply.github.com>
2022-10-05 19:20:08 -0300

    Update handleCommands.js

--- src/handlers/handleCommands.js
+++ src/handlers/handleCommands.js
@@ -15,24 +15,6 @@ module.exports = (client) => {
         }
     }
 
-    client.once('ready', () => {
-const fs = require('fs')
-
-module.exports = (client) => {
-    const commandsArray = []
-    const commandsFolders = fs.readdirSync('./src/commands');
-    for (const folders of commandsFolders) {
-        const commandsFiles = fs.readdirSync(`./src/commands/${folders}`)
-        for (const files of commandsFiles) {
-            if (!files.endsWith('.js')) return;
-            const command = require(`../commands/${folders}/${files}`)
-
-            commandsArray.push(command)
-
-            client.commands.set(command.name, command)
-        }
-    }
-
     client.once('ready',async() => {
  // SlashCommands em servidor específico
         // await client.guilds.cache
@@ -47,10 +29,3 @@ module.exports = (client) => {
         })
     })
 } 
-
-        console.log(`${commandsFolders} Está funcionando corretamente.`)
-        client.guilds.cache.forEach((guild) => {
-            guild.commands.set(commandsArray)
-        })
-    })
-}
