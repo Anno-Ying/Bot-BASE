0d4ccffc5bc61905894fc0ccd292653e4623cbec
Annoying <84549791+Anno-Ying@users.noreply.github.com>
2022-10-06 08:37:23 -0300

    Create Commands.js

---
+++ docs/client/modules/Commands.js
@@ -0,0 +1,16 @@
+const { embedColor, botClientID, ownerID, prefix, token } = require('../../config/settings.json');
+
+/* 
+    module.exports = {
+    name: 'command',
+    description: 'Base of command.',
+    category: "Name of category",    aliases: ["c", "command"],
+    ownerOnly: false,  // or true,
+    type: 1, 
+    options:
+    [{
+         name: 'Olá mundo',
+         type: 1,
+    }]
+/*
+
